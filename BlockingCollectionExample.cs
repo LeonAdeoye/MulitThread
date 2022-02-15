af83f78e1ae42cce09a5ff6fd0d303080becebe7
LeonAdeoye <leon.adeoye@gmail.com>
2022-02-15 14:57:04 +0800

    refactoring to use async await tasks

--- BlockingCollectionExample.cs
+++ BlockingCollectionExample.cs
@@ -9,28 +9,15 @@ namespace MulitThread
 {
     internal class BlockingCollectionExample
     {
-        public static void Main()
+        public static async void Main()
         {
             BlockingCollection<int> blockingCollection = new();
-            int count = 10;
-
-            Consumer consumer = new(blockingCollection, count);
-            Producer producer = new(blockingCollection, count);
-
-            // Create threads using Thread and ThreadStart:
-            //new Thread(new ThreadStart(consumer.Run)).Start();
-            //new Thread(new ThreadStart(producer.Run)).Start();
-
-            // Create parent-child thread using Task Parallel Library:
-            Task parentTask = Task.Factory.StartNew(() =>
-            {
-                Task.Factory.StartNew(() => consumer.Run(), TaskCreationOptions.AttachedToParent);
-                Task.Factory.StartNew(() => producer.Run(), TaskCreationOptions.AttachedToParent)
-                    .ContinueWith(t => Console.WriteLine("The consumer and producer are done with blocking collection."));
-            });
-
-            parentTask.Wait();
-
+            Consumer consumer = new(blockingCollection, 10);
+            Producer producer = new(blockingCollection, 10);
+            Task consumerTask = Task.Run(() => consumer.Run());
+            Task producerTask = Task.Run(() => producer.Run());
+            await Task.WhenAll(new List<Task> { consumerTask, producerTask });
+            Console.WriteLine("The consumer and producer are done with blocking collection.");
         }
     }
 }
